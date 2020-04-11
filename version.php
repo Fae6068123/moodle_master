caa0dacc6278578253d20a4d2980e30460ea43c9
Frederick <frederick.evans@warwick.ac.uk>
2020-04-11 17:11:28 +0100

    initia commit

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file is part of Moodle - http://moodle.org/
-//
+// add this line to bring files into moodle repository
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
